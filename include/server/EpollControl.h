7383a32b8115f764037b58fec37a7980d0c7d4ec
peace901 <443257245@qq.com>
2023-07-16 19:51:15 +0800

    修改后2

--- include/server/EpollControl.h
+++ include/server/EpollControl.h
@@ -2,7 +2,7 @@
  * @Author: peace901 443257245@qq.com
  * @Date: 2023-07-14 13:34:46
  * @LastEditors: peace901 443257245@qq.com
- * @LastEditTime: 2023-07-14 14:27:11
+ * @LastEditTime: 2023-07-16 19:44:56
  * @FilePath: /maxtub/include/server/EpollControl.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEpragmatic
  */
@@ -18,22 +18,13 @@
 class EpollControl{
   private:
     int epoll_fd;
-    double time_limit;
     int evs_num;
     struct epoll_event ev,*evs;
 
   public:
-    enum OP{
-      add = EPOLL_CTL_ADD,
-      del = EPOLL_CTL_DEL,
-      mod = EPOLL_CTL_MOD
-    };
-
     EpollControl(
-      double time_limit_,
       int evs_num_
     ):
-    time_limit(time_limit_),
     evs_num(evs_num_)
     {
       epoll_fd = epoll_create(1);
@@ -49,29 +40,47 @@ class EpollControl{
       }
     }
 
-
-    /// @brief 修改事件
+    /// @brief 
+    /// @param fd 描述符
     /// @param events 事件类型
-    /// @param fd 
-    /// @param op OP
-    /// @return 返回结果
-    int epoll_control(uint32_t events,int fd,int op){
+    /// @return 结果
+    int add(int fd,uint32_t events){
       ev.events = events;
       ev.data.fd = fd;
-      return epoll_ctl(epoll_fd,op,fd,&ev);
+      return epoll_ctl(epoll_fd,EPOLL_CTL_ADD,fd,&ev);
     }
+
+    /// @brief 
+    /// @param fd 描述符
+    /// @param events 事件类型
+    /// @return 结果
+    int del(int fd,uint32_t events){
+      ev.events = events;
+      ev.data.fd = fd;
+      return epoll_ctl(epoll_fd,EPOLL_CTL_DEL,fd,&ev);
+    }
+
+    /// @brief 
+    /// @param fd 描述符
+    /// @param events 事件类型
+    /// @return 结果
+    int mod(int fd,uint32_t events){
+      ev.events = events;
+      ev.data.fd = fd;
+      return epoll_ctl(epoll_fd,EPOLL_CTL_MOD,fd,&ev);
+    }
+
+    /// @brief epoll_wait
+    /// @param evs_ 存evs的地址
+    /// @param wait_time_limit wait返回的时间限制
+    /// @return 
+    int wait(struct epoll_event* evs_,int wait_time_limit){
+      int num = epoll_wait(epoll_fd,evs,evs_num,wait_time_limit);
+      evs_ = evs;
+      return num;
+    }
+
     
-    int epoll_wait(,int timeout=10){
-
-    }
-
-    int GetEventFd(int i){
-
-    }
-
-    int GetEvent(int i){
-
-    }
 
     
 };
--- include/server/FollowProcess.h
+++ include/server/FollowProcess.h
@@ -2,7 +2,7 @@
  * @Author: peace901 443257245@qq.com
  * @Date: 2023-07-12 14:57:59
  * @LastEditors: peace901 443257245@qq.com
- * @LastEditTime: 2023-07-16 19:26:06
+ * @LastEditTime: 2023-07-16 19:49:22
  * @FilePath: /maxtub/include/server/FollowProcess.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -82,14 +82,16 @@ class FollowProcess{
 
 
     void follow_process_start(){
+      //创建ep 最大返回数量10000
+      ep.reset(new EpollControl(10000));
       
-      if( lck->lock() ){
-        while(accpet);
-      }
+      struct epoll_event * evs;
 
-      int num = ep->wait();
-      for(int i)
-      
+      for(;;){
+        //抢锁
+
+        int num = ep->wait(evs,100);
+      }
     }
 
     void add_client_(int client_fd,struct sockaddr_in remote_addr){

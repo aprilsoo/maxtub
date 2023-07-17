ea04e01546a3163cc1c58758d5f6da28f28ca58a
peace901 <443257245@qq.com>
2023-07-17 14:48:29 +0800

    asd2314

--- include/server/FollowProcess.h
+++ include/server/FollowProcess.h
@@ -2,7 +2,7 @@
  * @Author: peace901 443257245@qq.com
  * @Date: 2023-07-12 14:57:59
  * @LastEditors: peace901 443257245@qq.com
- * @LastEditTime: 2023-07-17 14:33:42
+ * @LastEditTime: 2023-07-17 14:47:07
  * @FilePath: /maxtub/include/server/FollowProcess.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -129,7 +129,13 @@ class FollowProcess{
     }
 
     void deal_read(int fd){
-
+      int ret = clients[fd] -> read();
+      if(ret == -2){
+        deal_close();
+      }
+      else if(ret == 1){
+        ep->add(fd,)
+      }
     }
     
     void deal_send(int fd){
